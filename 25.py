efd64ef00846c980daa1edfcba843e80b353d73e
Srujan <srujansgovinagidad@gmail.com>
2026-09-21 12:04:37 +0530

    Alphabet rangoli

---
+++ 25.py
@@ -0,0 +1,12 @@
+﻿# Alphabet rangoli
+
+import string
+
+size = int(input())
+alphabet = string.ascii_lowercase
+width = 4 * size - 3
+
+for i in range(size - 1, -size, -1):
+    row = alphabet[size - 1:abs(i):-1] + alphabet[abs(i):size]
+    row = "-".join(row)
+    print(row.center(width, "-"))
