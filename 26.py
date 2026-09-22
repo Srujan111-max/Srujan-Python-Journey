82ec676c100aae416af450a9a1c625bbb604995a
Srujan <srujansgovinagidad@gmail.com>
2026-09-22 17:56:50 +0530

    Designer Door Mat

---
+++ 26.py
@@ -0,0 +1,23 @@
+﻿# Designer Door Mat 
+
+N,M = map(int, input().split())
+M = N * 3
+n = M
+mid = N//2
+high = 1
+for i in range(N):
+    if mid != i:
+        text = ".|."*high 
+        print(text.center(n,"-"))
+        high = high + 2
+        low = high - 2
+        j = i 
+    else:
+        print("WELCOME".center(n,"-"))
+        for j in range (N-j):
+            text = ".|."*low
+            print(text.center(n,"-"))
+            low = low - 2
+            if low < 1:
+                break
+        break
