489ac160128b19e1093a2b8d0d48ad4a91ecbb97
Srujan <srujansgovinagidad@gmail.com>
2026-09-25 00:29:49 +0530

    Set .union() Operation

---
+++ 29.py
@@ -0,0 +1,10 @@
+﻿# Set .union() Operation
+
+a = int(input())
+set_a = set(map(int,input().split()))
+b = int(input())
+set_b = set(map(int,input().split()))
+
+final_set = set_a.union(set_b)
+print(final_set)
+print(len(final_set))
